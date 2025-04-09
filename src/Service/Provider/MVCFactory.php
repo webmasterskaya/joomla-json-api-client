66b33fb699c9a753ef37af32b1b81fff5fd473c4
Artem Vasilev <kern.usr@gmail.com>
2025-04-09 23:02:18 +0300

    Сократил неймспейсы в коде

--- src/Service/Provider/MVCFactory.php
+++ src/Service/Provider/MVCFactory.php
@@ -10,9 +10,9 @@
 namespace Webmasterskaya\JsonApi\Client\Service\Provider;
 
 use Joomla\CMS\Cache\CacheControllerFactoryInterface;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormFactoryInterface;
 use Joomla\CMS\Mail\MailerFactoryInterface;
-use Joomla\CMS\MVC\Factory\ApiMVCFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\Router\SiteRouter;
 use Joomla\CMS\User\UserFactoryInterface;
@@ -21,22 +21,20 @@ use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
 use Webmasterskaya\JsonApi\Client\ClientFactoryInterface;
+use Webmasterskaya\JsonApi\Client\MVC\Factory\ApiMVCFactory;
+use Webmasterskaya\JsonApi\Client\MVC\Factory\MVCFactory as MVCFactoryClass;
 
 class MVCFactory implements ServiceProviderInterface
 {
 	/**
 	 * The extension namespace
 	 *
-	 * @var  string
-	 *
 	 * @since   4.0.0
 	 */
-	private $namespace;
+	private string $namespace;
 
 	/**
 	 * MVCFactory constructor.
-	 *
-	 * @param   string  $namespace  The namespace
 	 *
 	 * @since   4.0.0
 	 */
@@ -54,15 +52,18 @@ class MVCFactory implements ServiceProviderInterface
 	 *
 	 * @since   4.0.0
 	 */
-	public function register(Container $container)
+	public function register(Container $container): void
 	{
 		$container->set(
 			MVCFactoryInterface::class,
 			function (Container $container) {
-				if (\Joomla\CMS\Factory::getApplication()->isClient('api')) {
-					$factory = new \Webmasterskaya\JsonApi\Client\MVC\Factory\ApiMVCFactory($this->namespace);
-				} else {
-					$factory = new \Webmasterskaya\JsonApi\Client\MVC\Factory\MVCFactory($this->namespace);
+				if (Factory::getApplication()->isClient('api'))
+				{
+					$factory = new ApiMVCFactory($this->namespace);
+				}
+				else
+				{
+					$factory = new MVCFactoryClass($this->namespace);
 				}
 
 				$factory->setFormFactory($container->get(FormFactoryInterface::class));
